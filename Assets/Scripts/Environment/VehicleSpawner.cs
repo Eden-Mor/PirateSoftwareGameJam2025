fb9bf2ac7b32066ba1e3e5377451c34cca825ae9
Andy Freeborough <andy@freeborough.com>
2025-01-30 13:10:44 +0000

    Vehicle spawn on world initialisation fixed.
    Road tiles to spawn on are no longer picked completely at random - when a vehicle is spawned on a road tile that road tile is removed from the pool of tiles that can be spawned on in this initial process, preventing vehicles from spawning inside each other.
    
    This does not fix the general re-spawning issue, which is the next thing to fix!

--- Assets/Scripts/Environment/VehicleSpawner.cs
+++ Assets/Scripts/Environment/VehicleSpawner.cs
@@ -37,31 +37,47 @@ public class VehicleSpawner : MonoBehaviour
 		// TODO: A much nicer solution than this!
 		if(!spawned && world.chunks.Count > 0)
 		{
-			for(int i = 0; i < numberOfVehicles; i++)
-				SpawnVehicle();
-
+			InitialSpawn();
 			spawned = true;
 		}
 	}
 
 	/// <summary>
-	/// Spawns a random vehicle on a random road tile.
-	/// TODO: Have this the behaviour of the initial spawn, though eliminating tiles each time so we don't collide.
+	/// Spawns vehicles all across the world on initial startup, ensuring that two vehicles don't 
+	/// spawn in the same position.
 	/// </summary>
-	public void SpawnVehicle()
+	public void InitialSpawn()
 	{
-		// TODO: Take into account player view frustrum, where we've recently spawned vehicles, etc.
-		var tile = world.GetRandomRoadTile();
+		var remainingRoadTiles = new List<Tile>( world.roadTiles );
 
+		for(int i = 0; i < numberOfVehicles; i++)
+		{
+			if(remainingRoadTiles.Count > 0)
+			{
+				// Take a random road tile from the remaining list.
+				var tile = remainingRoadTiles[ UnityEngine.Random.Range( 0, remainingRoadTiles.Count ) ];
+				remainingRoadTiles.Remove( tile );
+				SpawnVehicleOnTile( tile );
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	/// <summary>
+	/// Spawns a vehicle on the specified tile.
+	/// </summary>
+	/// <param name="tile">The tile to spawn the vehicle on.</param>
+	public void SpawnVehicleOnTile( Tile tile )
+	{
 		// Get a Tile Path from the tile, which we'll have the vehicle follow.
 		TilePath tilePath = tile.RandomTilePath();
-
-	//	Debug.Log( "VehicleSpawner > SpawnVehicle > tile.transform.position: " + tile.transform.position );
 
 		if(tilePath != null)
 		{
 			// Spawn the Vehicle and set it on the tile path.
-			// TODO: Weights.
 			GameObject vehiclePrefab = vehiclePrefabs[ UnityEngine.Random.Range( 0, vehiclePrefabs.Length ) ];
 			GameObject vehicleObject = Instantiate( vehiclePrefab );
 			vehicleObject.transform.SetParent( vehiclesParent.transform, false );
@@ -71,6 +87,16 @@ public class VehicleSpawner : MonoBehaviour
 			vehicle.tilePath = tilePath;
 			vehicle.spawner = this;
 		}
+	}
+
+	/// <summary>
+	/// Spawns a random vehicle on a random road tile.
+	/// </summary>
+	public void SpawnVehicle()
+	{
+		// TODO: Take into account player view frustrum, where we've recently spawned vehicles, etc.
+		var tile = world.GetRandomRoadTile();
+		SpawnVehicleOnTile( tile );
 	}
 
 	/// <summary>
